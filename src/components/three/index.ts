050c8885e9f0715a1e628d548f23b2e2290f9a75
Sharad Mayank Sinha <ghostkwebb@gmail.com>
2025-08-18 23:03:46 +0530

    Removed useless three js files

--- src/components/three/index.ts
+++ src/components/three/index.ts
@@ -1,4 +1,6 @@
 // Three.js components exports
-export { default as ThreeBackground } from './ThreeBackground';
+export { default as EpicGamerBackground } from './EpicGamerBackground';
 export { default as ThreeErrorBoundary } from './ThreeErrorBoundary';
 export { default as ParticleTransitions } from './ParticleTransitions';
+export { default as ExtremeOptimizedThreeBackground } from './ExtremeOptimizedThreeBackground';
+export { default as ExtremeOptimizedParticleSystem } from './ExtremeOptimizedParticleSystem';
