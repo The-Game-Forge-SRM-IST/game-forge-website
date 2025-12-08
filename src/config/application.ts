47f84c713154e902bf5a9e2e42d5f22d3fd837c4
Sharad Mayank Sinha <ghostkwebb@gmail.com>
2025-12-08 21:20:00 +0530

    bug fix (forms)

--- src/config/application.ts
+++ src/config/application.ts
@@ -9,14 +9,14 @@
 export const APPLICATION_CONFIG = {
   // 🎯 CHANGE THIS VALUE TO OPEN/CLOSE APPLICATIONS
   APPLICATION_OPEN: false,
-  
+
   // Message shown when applications are closed
   CLOSED_MESSAGE: {
     title: "Applications Currently Closed",
     description: "Thank you for your interest in joining The Game Forge! Applications are currently closed, but we'll be opening them again soon. Follow our social media or check back later for updates.",
     reopenMessage: "Applications will reopen soon. Stay tuned!"
   },
-  
+
   // Message shown when applications are open
   OPEN_MESSAGE: {
     title: "Join The Game Forge",
